bcab3f35ec55e0b842e55344b1588b81baf5d4d2
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-13 15:19:04 -0400

    add ajax fetch lesson complete

--- 03 fetch fetch api/app.js
+++ 03 fetch fetch api/app.js
@@ -1,32 +1,21 @@
-//JSON
-const btn = document.querySelector('.btn');
+//
 const url = './api/people.json';
 
-btn.addEventListener('click', () => {
-  getData(url);
-});
+// const response = fetch(url);
 
-function getData(url) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      const displayData = data
-        .map((item) => {
-          return `<p>${item.name}</p>`;
-        })
-        .join('');
-      const element = document.createElement('div');
-      element.innerHTML = displayData;
-      document.body.appendChild(element);
-    } else {
-      console.log({
-        status: xhr.status,
-        text: xhr.statusText,
-        state: xhr.readyState,
-      });
-    }
-  };
-  xhr.send();
-}
+// fetch(url)
+//   .then((resp) => {
+//     //reponse obejct
+//     return resp.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
