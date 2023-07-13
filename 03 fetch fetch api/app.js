536e7135d52b175f2110aa39bc56a583ff3b1bad
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-13 14:50:25 -0400

    add ajax fetch lesson

---
+++ 03 fetch fetch api/app.js
@@ -0,0 +1,32 @@
+//JSON
+const btn = document.querySelector('.btn');
+const url = './api/people.json';
+
+btn.addEventListener('click', () => {
+  getData(url);
+});
+
+function getData(url) {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      const displayData = data
+        .map((item) => {
+          return `<p>${item.name}</p>`;
+        })
+        .join('');
+      const element = document.createElement('div');
+      element.innerHTML = displayData;
+      document.body.appendChild(element);
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        state: xhr.readyState,
+      });
+    }
+  };
+  xhr.send();
+}
