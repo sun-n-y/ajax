252d9069c0e1f26da76f7181f4f2b1cc5b97d396
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-13 16:09:33 -0400

    add ajax fetch with function complete

--- 05 fetch with function/app.js
+++ 05 fetch with function/app.js
@@ -1,7 +1,27 @@
-//big picture
+//fetch example with function
+
 const url = './api/people.json';
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      showData(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+const showData = (data) => {
+  const element = document.createElement('div');
+  element.innerHTML = data
+    .map((item) => {
+      return `<p>${item.name}</p>`;
+    })
+    .join('');
+  document.body.appendChild(element);
+  // return;
+};
