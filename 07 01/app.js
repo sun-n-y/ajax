37da43e34736d73f2c014fc3ec44a335994495cd
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-06 12:09:42 -0400

    add json lesson

--- 07 01/app.js
+++ 07 01/app.js
@@ -1,21 +1,28 @@
 //
 const btn = document.querySelector('.btn');
+const url = './api/people.json';
 
 btn.addEventListener('click', () => {
-  getData();
+  getData(url);
 });
 
-function getData() {
+function getData(url) {
   const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', './api/sample.txt');
+  xhr.open('GET', url);
 
   xhr.onreadystatechange = function () {
-    //   console.log(xhr);
+    // console.log(xhr);
     if (xhr.readyState === 4 && xhr.status === 200) {
-      const text = document.createElement('p');
-      text.textContent = xhr.responseText;
-      document.body.appendChild(text);
+      const data = JSON.parse(xhr.responseText);
+      const displayData = data
+        .map((item) => {
+          return `<p>${item.name}</p>`;
+        })
+        .join('');
+      const element = document.createElement('div');
+      element.innerHTML = displayData;
+      document.body.appendChild(element);
     } else {
       console.log({
         status: xhr.status,
