36c5db7b01ff433a477ce062dcf106e280086f68
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-06 13:37:23 -0400

    add fetch lesson

--- 07 01/app.js
+++ 07 01/app.js
@@ -1,36 +1,10 @@
 //
-const btn = document.querySelector('.btn');
 const url = './api/people.json';
 
-btn.addEventListener('click', () => {
-  getData(url);
-});
+// const reponse = fetch(url);
+// console.log(reponse);
 
-function getData(url) {
-  const xhr = new XMLHttpRequest();
-
-  xhr.open('GET', url);
-
-  xhr.onreadystatechange = function () {
-    // console.log(xhr);
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
-      const displayData = data
-        .map((item) => {
-          return `<p>${item.name}</p>`;
-        })
-        .join('');
-      const element = document.createElement('div');
-      element.innerHTML = displayData;
-      document.body.appendChild(element);
-    } else {
-      console.log({
-        status: xhr.status,
-        text: xhr.statusText,
-        state: xhr.readyState,
-      });
-    }
-  };
-
-  xhr.send();
-}
+fetch(url)
+  .then((resp) => resp.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
